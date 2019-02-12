60e628519dc704ab78389e636675d40da69a1534
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-02-12 14:14:23 +0000

    Add readiness probe to the DB container
    Move the readiness and schema upgrade from init containers in the
    API pod to the database side so that the database service is self
    contained and the DB pod readiness matches the actual DB service
    readiness.
    
    This is implemented using a feature of the postgres docker image
    that permits running init scripts when the DB is started and
    listening to socket but not yet via the network interface.
    The docker image starts the DB, runs the migration scripts mounted
    from the git repo and then starts listening on the network
    interface. The readinessProbe and livenessProbe check the database
    readiness via the cluster DB hostname and port.
    
    The database docker image has been refactored to be as thin as
    possible.

--- images/database/run_db_migrations.sh
+++ images/database/run_db_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
 set -x
 
-DB_URI="postgresql+psycopg2://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB_NAME"
-/usr/local/bin/subunit2sql-db-manage --database-connection $DB_URI upgrade head
+# Connect on socket to the local DB
+DB_URI="postgresql+psycopg2:///$DB_NAME?user=$POSTGRES_USER"
+/usr/bin/subunit2sql-db-manage --database-connection $DB_URI upgrade head
