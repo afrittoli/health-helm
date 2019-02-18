ce27c93196a88b0c100a34c77cea9703e2c9ea6f
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-02-18 11:46:15 +0000

    Fix folders in API and FE images

--- images/frontend/run.sh
+++ images/frontend/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir -p /var/run/uwsgi
+mkdir -p /var/log/supervisor
+mkdir -p /var/log/nginx
+
 echo "{" > /usr/share/nginx/html/config.json
 echo "  \"apiRoot\": \"$API_URL\"" >> /usr/share/nginx/html/config.json
 echo "}" >> /usr/share/nginx/html/config.json
